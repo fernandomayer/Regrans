73b21661fb8c4f4936c327fdb19f9e8cb8ec9b91
Rodrigo Sant'Ana <oc.rodrigosantana@gmail.com>
2012-02-15 12:49:27 -0200

    Script atualizado

--- inf.point.R
+++ inf.point.R
@@ -1,21 +1,29 @@
-dados <- read.csv2("FLA.csv")
+## Entrada dos dados
+dados <- read.csv2("FLA.csv") # Base de dados teste - Deep-sea crabs
 
+# Plot logaritmizados das variáveis Largura do Abdome (LA) em função da
+# Largura da Carapaça (LC)
 plot(log(LA) ~ log(LC), data=dados)
 
+# Histograma das variáveis Largura do Abdome (LA) e Largura da Carapaça
+# (LC)
 par(mfrow=c(1,2))
 hist(log(dados$LA))
 hist(log(dados$LC))
 par(mfrow=c(1,1))
 
+# Novo objeto contendo as variáveis LC e LA logaritmizadas e ordenadas
+# em ordem crescente de largura da carapaça
 dados2 <- dados[order(dados$LC),]
 dados2[,1] <- log10(dados2[,1])
 dados2[,2] <- log10(dados2[,2])
 
-
+## Criação de um novo objeto para receber os dados do looping a seguir
 saida <- data.frame(SQResL = numeric(0), aL = numeric(0), bL =
                     numeric(0), nL = numeric(0), SQResR = numeric(0), aR
                     = numeric(0), bR = numeric(0), nR = numeric(0))
 
+## Looping para as estimação das regressões lineares iterativas
 for(i in 3:(nrow(dados2)-2)){
     regL <- lm(LA[1:i] ~ LC[1:i], data = dados2)
     regR <- lm(LA[(i+1):(nrow(dados2)-2)] ~ LC[(i+1):(nrow(dados2)-2)],
@@ -28,4 +36,7 @@ for(i in 3:(nrow(dados2)-2)){
                     coef(regR)[1], bR = coef(regR)[2], nR =
                     length(dados2$LA[(i+1):(nrow(dados2)-2)])))
 }
-row.names(saida) <- NULL
+
+row.names(saida) <- NULL # Preenche os nomes das linhas com uma
+                         # sequencia númerica continua iniciando de 1.
+
