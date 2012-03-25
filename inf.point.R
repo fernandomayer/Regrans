60de42fa914cc902b5a3ed58a7f65ab6a7fe8f4c
Rodrigo Sant'Ana <oc.rodrigosantana@gmail.com>
2012-03-25 04:26:36 -0300

    Equivalent to Regrans analisys developed in BASIC language

--- inf.point.R
+++ inf.point.R
@@ -28,8 +28,9 @@ plot(AW ~ CW, data = dados2)
 plot(lAW ~ lCW, data = dados2)
 par(mfrow=c(1,1))
 
-teste <- lm(lAW ~ lCW, data = dados2)
-coef(teste)
+## Linear model (regression) for total data
+mod.total <- lm(lAW ~ lCW, data = dados2)
+coef(mod.total)
 
 ## Calls a new object to receive data from looping
 saida <- data.frame(SSResL = numeric(0), aL = numeric(0),
@@ -37,8 +38,8 @@ saida <- data.frame(SSResL = numeric(0), aL = numeric(0),
                     SSResR = numeric(0), aR = numeric(0),
                     bR = numeric(0), nR = numeric(0))
 
-## define n.min = the minimum number of points for a regression
-n.min <- 3
+## Define n.min = the minimum number of points for a regression
+n.min <- 5
 
 ## Looping to estimate the iteractive linear models (regressions)
 for(i in n.min:(nrow(dados2)-n.min)){
@@ -60,7 +61,25 @@ for(i in n.min:(nrow(dados2)-n.min)){
 # Remove rownames
 row.names(saida) <- NULL
 
-## check the minimum value of SSR
-saida[saida$SSResL == min(saida$SSResL), ]
-saida[saida$SSResR == min(saida$SSResR), ]
+## Calls a new column with Sums of Sum Squared Residuals of right and
+## left models.
+saida$sumSSResLR <- saida$SSResL + saida$SSResR
+
+## Shows a line from saida's object with the minimum sumSSRes of right
+## and left models; And apresent the sum of squared residuals of total
+## model
+saida[saida$sumSSResLR == min(saida$sumSSResLR),]
+
+sum.total.model <- sum(residuals(mod.total)^2)
+sum.total.model
+
+### Graphics for the piecewise regression
+require(lattice)
+
+xyplot(AW ~ CW, data = dados, xlab = "Carapace Width (cm)", ylab =
+       "Abdome Width (cm)", col = "black", panel =
+       function(x, y, ...) {
+
+       }
+       )
 
